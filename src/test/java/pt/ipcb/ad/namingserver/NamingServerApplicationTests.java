a249123b27c0c3bcf69933a6af743c7461c79bba
Eric Lima <eric.george.lima@gmail.com>
2025-04-10 06:49:03 +0100

    initial

---
+++ src/test/java/pt/ipcb/ad/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ipcb.ad.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
